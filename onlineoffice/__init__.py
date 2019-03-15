bcae5afe69c9257065d8c2f74c37bcb4c9195aae
Oumourin <maolinwang1998@gmail.com>
2019-03-15 23:43:40 +0800

    Create Framework

---
+++ onlineoffice/__init__.py
@@ -0,0 +1,6 @@
+DEBUG = True
+SECRET_KEY = 'A0ZrNKFEHUF9*EWFH&*h(H&(#Y(#&HDWDH(/,?RT'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = r"mysql+pymysql://root:123456@127.0.0.1:3306/book?charset=utf8mb4"
+# SQLALCHEMY_DATABASE_URI = r"mysql+pymysql://root:98a93U9ZNha7B7hs@47.93.242.157:3306/book?charset=utf8mb4"
